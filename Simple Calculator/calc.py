f60638c8ff12f06261bf33ae97f84149fdb8bb07
Angshuman Sengupta <senguptaangshuman17@gmail.com>
2020-10-01 12:55:14 +0530

    Simple Calculator added

---
+++ Simple Calculator/calc.py
@@ -0,0 +1,52 @@
+# Program make a simple calculator
+
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # Take input from the user
+    choice = input("Enter choice(+|-|*|/|E): ")
+
+    # Check if choice is one of the four options
+    if choice in ('+', '-', '*', '/','E'):
+        if choice == 'E':
+            print("Calculator Shutting down......")
+            break
+
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '+':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '-':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '*':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '/':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+    else:
+        print("Invalid Input")
