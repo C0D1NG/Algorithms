2b1b737dac01ff255cc09ade070c276ede848d6a
Mansi Mishra <34184275+mansim3006@users.noreply.github.com>
2020-10-29 22:09:21 +0530

    Create MajorElements.java
    element having more than n/2 occurrene in an array

---
+++ Others/MajorElements.java
@@ -0,0 +1,53 @@
+/******************************************************************************
+Major Element-> element having more than n/2 occurrene in an array
+Leetcode Link-> https://leetcode.com/problems/majority-element/
+*******************************************************************************/
+import java.util.Scanner;
+
+public class MajorElement
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++) {
+		    arr[i]=sc.nextInt();
+		}
+		int maxElement=arr[0];   //initially let major element
+		int counter=1;
+		
+		//check for possible major element
+		for(int i=1; i<n; i++) {
+            if(arr[i]==maxElement) {
+                counter++;
+            } else if(counter>0) {
+                counter--;
+            } else {
+                counter=1;
+                maxElement=arr[i];
+            } 
+		}
+		int maxOccur=0;
+		if(counter>0) {
+		    for(int element:arr) {
+		        if(element==maxElement) 
+		            maxOccur++;
+		    }
+		}
+		if(maxOccur>n/2) {
+		    System.out.println("Major Element is: "+maxElement);
+		}else {
+		    System.out.println(" No Major Element is");
+		}
+	}
+}
+
+//Input-1: 6
+//         2 2 2 4 5 2 
+//Output-1: Major Element is: 2
+
+//Input-2: 9
+//         4 4 4 4 5 2 4 4 5 
+//Output-2: Major Element is: 4
+
+//Time Complexity-O(n)
